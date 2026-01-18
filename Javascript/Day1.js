3ef22bf3110fc116242450c5703a007b493db8c3
akshayku55 <akshay24ku@gmail.com>
2026-01-18 23:09:44 +0530

    Day1 of JS: Built Cart System and Rock Paper Scissor Game

---
+++ Javascript/Day1.js
@@ -0,0 +1,51 @@
+// // console.log(10+8*3+5);
+// // // 2b 
+// // console.log((10+8*3+5)/3)
+// // // 2c
+// // console.log((1850+750*2)/100)
+// // //2d
+// // console.log(Math.round(3350*0.1)/100)
+// // // 2e
+// // console.log(Math.round(3350*0.2)/100)
+
+// //2i
+// // console.log((1090+2095+1899)/100);
+// // console.log((1090+2095+1899)/100+((499*3)/100))
+// // console.log(Math.round((1090+2095+1899+499*3)*0.1)/100);
+
+// // Math floor rounds down the Integer
+// //2j
+// console.log(Math.floor(2.8))
+// //2k
+// console.log(Math.ceil(2.2));
+
+// // 2l
+// // celsius to fahrenheit
+// console.log((14*9/5)+32)
+
+// // fah to cel
+// console.log((86-32)*5/9 )
+
+// // 2n
+// console.log((-5*9/5)+32)
+
+
+// Strings
+// console.log("Akshay");
+// console.log("My name is"+ "Akshay"); 
+// console.log(`Total Cost: $${5+3}`);
+// console.log(`Total cost: $${(599+295)/100}\nThank you come again!`)
+// console.log(2+"Akshays") // type coercion 
+
+// let name = "Akshay";
+// console.log(`My name is ${name}`);
+// let cost = 5+3+9;
+// console.log(`The cost ${cost}`);
+// let tax = cost * 0.1;
+// let total = cost + tax;
+// console.log(total);
+
+// amazon
+let cost = 5+10+120;
+let tax = cost * 0.1;
+console.log(`the cost is ${cost} and ${tax} and total is ${cost+tax}`);
